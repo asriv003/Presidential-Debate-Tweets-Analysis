0fe010522fe014bf19e0909c76f79fafcfeb8b97
Abhishek Kumar Srivastava <asriv003@ucr.edu>
2016-11-13 20:16:05 -0800

    parsing updates

--- Parse_data.py
+++ Parse_data.py
@@ -2,8 +2,6 @@ import json
 import sys
 import collections
 
-f = open('parsed_data', 'w')
-
 # if len(sys.argv) > 1:
 #     line_generator = open(sys.argv[1])
 # else:
@@ -14,35 +12,54 @@ f = open('parsed_data', 'w')
 
 def parse_and_insert(tweet):
     #parsed tweet
-    if tweet['is_quote_status'] and 'quoted_status' in tweet:
+    if tweet.get('is_quote_status') and 'quoted_status' in tweet:
         #Insert quoted status
-        parse_and_insert(tweet['quoted_status'])
-    parsed_tweet = {'created_at': tweet['created_at']}
-    parsed_tweet.update({'tweet_id': tweet['id']})
-    parsed_tweet.update({'tweet_text': tweet['text']})
-    parsed_tweet.update({'tweet_source': tweet['source']})
-    parsed_tweet.update({'in_reply_to_status_id': tweet['in_reply_to_status_id']})
-    parsed_tweet.update({'in_reply_to_user_id': tweet['in_reply_to_user_id']})
-    parsed_tweet.update({'in_reply_to_screen_name': tweet['in_reply_to_screen_name']})
-    parsed_tweet.update({'tweet_coordinates': tweet['coordinates']})
+        parse_and_insert(tweet.get('quoted_status'))
+    parsed_tweet = {'created_at': tweet.get('created_at')}
+    parsed_tweet.update({'tweet_id': tweet.get('id')})
+    parsed_tweet.update({'tweet_text': tweet.get('text')})
+    parsed_tweet.update({'tweet_source': tweet.get('source')})
+    parsed_tweet.update({'in_reply_to_status_id': tweet.get('in_reply_to_status_id')})
+    parsed_tweet.update({'in_reply_to_user_id': tweet.get('in_reply_to_user_id')})
+    parsed_tweet.update({'in_reply_to_screen_name': tweet.get('in_reply_to_screen_name')})
+    parsed_tweet.update({'tweet_coordinates': tweet.get('coordinates')})
     #check if is_quoted_status is true but no quoted_status object is present
-    if tweet['is_quote_status'] and 'quoted_status' in tweet:
-        parsed_tweet.update({'quoted_status_id': tweet['quoted_status_id']})
-    else:
-        parsed_tweet.update({'quoted_status_id': 0})
-    
-    parsed_tweet.update({'retweet_count': tweet['retweet_count']})
-    parsed_tweet.update({'favorite_count': tweet['favorite_count']})
-    if 'timestamp_ms' in tweet:
-        parsed_tweet.update({'timestamp_ms': tweet['timestamp_ms']})
-    else:
-        parsed_tweet.update({'timestamp_ms': "0"})
+    parsed_tweet.update({'quoted_status_id': tweet.get('quoted_status_id',0)})
+    parsed_tweet.update({'retweet_count': tweet.get('retweet_count',0)})
+    parsed_tweet.update({'favorite_count': tweet.get('favorite_count',0)})
+    parsed_tweet.update({'timestamp_ms': tweet.get('timestamp_ms',"0")})
     #Users Object
-    parsed_tweet.update({'user': tweet['user']})
+    user_info = tweet.get('user')
+    parsed_tweet.update({'user_id': user_info.get('id')})
+    parsed_tweet.update({'user_name': user_info.get('name')})
+    parsed_tweet.update({'user_screen_name': user_info.get('screen_name')})
+    parsed_tweet.update({'user_verified': user_info.get('verified',"false")})
+    parsed_tweet.update({'user_description': user_info.get('description')})
+    parsed_tweet.update({'user_followers_count': user_info.get('followers_count',0)})
+    parsed_tweet.update({'user_friends_count': user_info.get('friends_count',0)})
+    parsed_tweet.update({'user_location': user_info.get('location',"")})
     #Places Object
-    parsed_tweet.update({'place': tweet['place']})
+    place_info = tweet.get('place')
+    if place_info != None:
+        parsed_tweet.update({'place_country': place_info.get('country',"")})
+        parsed_tweet.update({'place_full_name': place_info.get('full_name',"")})
+        parsed_tweet.update({'place_type': place_info.get('place_type',"")})
+        parsed_tweet.update({'place_coordinates': place_info.get('bounding_box',[]).get('coordinates',[])})
+    else:
+        parsed_tweet.update({'place_country': ""})
+        parsed_tweet.update({'place_full_name': ""})
+        parsed_tweet.update({'place_type': ""})
+        parsed_tweet.update({'place_coordinates': []})
     #Entities Object
-    parsed_tweet.update({'entities': tweet['retweet_count']})
+    entities = tweet.get('entities')
+    hash_tags = []
+    for item in entities.get('hashtags'):
+        hash_tags.append(item.get('text'))
+    parsed_tweet.update({'hash_tags': hash_tags})
+    user_mentions = []
+    for item in entities.get('user_mentions'):
+        user_mentions.append(item.get('screen_name'))
+    parsed_tweet.update({'user_mentions': user_mentions})
     #sorting accourding to key fields in dictonary
     od_parsed_tweet = collections.OrderedDict(sorted(parsed_tweet.items()))
 
